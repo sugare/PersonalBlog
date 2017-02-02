6e4dc837788b7674f69f522f609b3f2b7da0adc0
song <30733705@qq.com>
2017-02-02 13:20:00 +0800

    thumbs-up

--- sql
+++ sql
@@ -1,9 +1,9 @@
 CREATE DATABASE blog DEFAULT CHARACTER SET utf8;
-USE BLOG;
+USE blog;
 CREATE TABLE blog(
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  /*文章id*/
 `title` VARCHAR(100) NOT NULL,  /*文章标题*/
-`date` DATE NOT NULL,  /*文章日期*/
+`date` DATE NOT NULL DEFAULT '1996-02-01',  /*文章日期*/
 `identity` VARCHAR(50) DEFAULT 'other',  /*类别*/
 `essay` MEDIUMTEXT, /*文章内容*/
 `view` INT NOT NULL DEFAULT 0  /*文件浏览数*/
--- static/js/dist.js
+++ static/js/dist.js
@@ -1,6 +1,7 @@
 /**
  * Created by song on 17-1-23.
  */
+//日历
 function startTicker() {
     $("#news").find("li:first").slideUp(function () {
         $(this).appendTo($("#news")).slideDown();
@@ -297,4 +298,29 @@ var CalendarHandler = {
         }, 1000);
 
     }
-}
+};
+
+//点赞
+    $(function(){
+    $("#praise").click(function(){
+        var praise_img = $("#praise-img");
+        var text_box = $("#add-num");
+        var praise_txt = $("#praise-txt");
+        var num=parseInt(praise_txt.text());
+        if(praise_img.attr("src") == ("/static/images/yizan.png")){
+            $(this).html("<img src='/static/images/zan.png' id='praise-img' class='animation' />");
+            praise_txt.removeClass("hover");
+            text_box.show().html("<em class='add-animation'>-1</em>");
+            $(".add-animation").removeClass("hover");
+            num -=1;
+            praise_txt.text(num)
+        }else{
+            $(this).html("<img src='/static/images/yizan.png' id='praise-img' class='animation' />");
+            praise_txt.addClass("hover");
+            text_box.show().html("<em class='add-animation'>+1</em>");
+            $(".add-animation").addClass("hover");
+            num +=1;
+            praise_txt.text(num)
+        }
+    });
+});
